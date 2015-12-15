e2058bb090155eda77547dff7f750322d7e35b10
MartinCoderDojo <martin@coderdojoscotland.com>
2015-12-15 11:39:55 +0000

    Create Auld Lang Syne.rb

---
+++ Auld Lang Syne.rb
@@ -0,0 +1,147 @@
+use_synth :piano
+# Bar 1
+play 50
+sleep 0.5
+
+# Bar 2
+play 55
+sleep 0.75
+play 55
+sleep 0.25
+play 55
+sleep 0.5
+play 59
+sleep 0.5
+
+# Bar 3
+play 57
+sleep 0.75
+play 55
+sleep 0.25
+play 57
+sleep 0.5
+play 59
+sleep 0.5
+
+# Bar 4
+play 55
+sleep 0.75
+play 55
+sleep 0.25
+play 59
+sleep 0.5
+play 62
+sleep 0.5
+
+# Bar 5
+play 64
+sleep 1.5
+play 64
+sleep 0.5
+
+# Bar 6
+play 62
+sleep 0.75
+play 59
+sleep 0.25
+play 59
+sleep 0.5
+play 55
+sleep 0.5
+
+# Bar 7
+play 57
+sleep 0.75
+play 55
+sleep 0.25
+play 57
+sleep 0.5
+play 59
+sleep 0.5
+
+# Bar 7
+play 55
+sleep 0.75
+play 52
+sleep 0.25
+play 52
+sleep 0.5
+play 50
+sleep 0.5
+
+# Bar 8
+play 55
+sleep 0.5
+sleep 0.5
+play 64
+sleep 0.5
+
+# Bar 9
+play 62
+sleep 0.75
+play 59
+sleep 0.25
+play 59
+sleep 0.75
+play 55
+sleep 0.25
+
+# Bar 10
+play 57
+sleep 0.75
+play 55
+sleep 0.25
+play 57
+sleep 0.5
+play 64
+sleep 0.5
+
+# Bar 11
+play 62
+sleep 0.75
+play 59
+sleep 0.25
+play 59
+sleep 0.75
+play 62
+sleep 0.25
+
+# Bar 12
+play 64
+sleep 1.5
+play 64
+sleep 0.5
+
+# Bar 13
+play 62
+sleep 0.75
+play 59
+sleep 0.25
+play 59
+sleep 0.5
+play 55
+sleep 0.5
+
+# Bar 14
+play 57
+sleep 0.75
+play 55
+sleep 0.25
+play 57
+sleep 0.5
+play 59
+sleep 0.5
+
+# Bar 15
+play 55
+sleep 0.75
+play 52
+sleep 0.25
+play 52
+sleep 0.5
+play 50
+sleep 0.5
+
+# Bar 16
+play 55
+sleep 1.5
