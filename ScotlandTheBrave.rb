43bec527cb389f77a0b8ffedda2f7daa97e0d605
MartinCoderDojo <martin@coderdojoscotland.com>
2016-01-07 14:04:10 +0000

    Update ScotlandTheBrave.rb
    Split the notes into bars

--- ScotlandTheBrave.rb
+++ ScotlandTheBrave.rb
@@ -1,10 +1,13 @@
 use_synth :piano
+# Bar 1
 play 50
 sleep 0.5
 play 50
 sleep 0.375
 play 52
 sleep 0.125
+
+# Bar 2
 play 53
 sleep 0.25
 play 50
@@ -13,12 +16,16 @@ play 53
 sleep 0.25
 play 57
 sleep 0.25
+
+# Bar 3
 play 62
 sleep 0.5
 play 62
 sleep 0.375
 play 60
 sleep 0.125
+
+# Bar 4
 play 62
 sleep 0.25
 play 57
@@ -27,12 +34,16 @@ play 53
 sleep 0.25
 play 50
 sleep 0.25
+
+# Bar 5
 play 55
 sleep 0.5
 play 59
 sleep 0.375
 play 55
 sleep 0.125
+
+# Bar 6
 play 53
 sleep 0.25
 play 57
@@ -41,12 +52,16 @@ play 53
 sleep 0.25
 play 50
 sleep 0.25
+
+# Bar 7
 play 52
 sleep 0.5
 play 57
 sleep 0.375
 play 59
 sleep 0.125
+
+# Bar 8
 play 57
 sleep 0.25
 play 55
